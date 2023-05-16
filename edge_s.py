0d23d3c3c3f01404490d2e1c6ec4d3ba3f81f482
Chalatsorn <88487459+Chalatsorn@users.noreply.github.com>
2023-05-16 22:00:37 +0700

    Add files via upload

---
+++ edge_s.py
@@ -0,0 +1,38 @@
+
+import cv2
+import numpy as np
+import convolution
+import time
+
+# Read and resize the image
+img = cv2.imread("Resources/mountain_image.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img, (500, 500))
+
+# Add padding to the image
+img = np.pad(img, [(1, 1), (1, 1)], mode='constant', constant_values=0)
+
+# Display the original image
+cv2.imshow("img", img)
+cv2.waitKey(0)
+
+# Initialize arrays for y and x gradients
+imgy = np.zeros(shape=(img.shape[0], img.shape[1]))
+imgx = np.zeros(shape=(img.shape[0], img.shape[1]))
+
+# Define the Laplacian filter
+laplace = [[0, -1, 0],
+           [-1, 4, -1],
+           [0, -1, 0]]
+
+# Perform convolution on the image with the Laplacian filter
+start = time.time()
+final = convolution.convolve(
+    img, 1, img.shape[0]-1, 1, img.shape[1]-1, laplace)
+end = time.time()
+
+# Print the time taken for convolution
+print("time taken: ", end-start)
+
+# Display the resulting image
+cv2.imshow("img", final/255)
+cv2.waitKey(0)
