01e16ccd8e77c95b417d4125edb012569f10ffe8
Vitchawong <118838766+Vitchawong@users.noreply.github.com>
2023-05-17 16:18:55 +0700

    Update graph_runtime.py

--- graph_runtime.py
+++ graph_runtime.py
@@ -1,21 +1,15 @@
-# runtime of parallel
 import matplotlib.pyplot as plt
 
 %matplotlib inline
 
 
 
-a = [200, 300, 500, 800]
-b = [1.3155276775360107,1.529815673828125,1.8073720932006836, 2.2616989612579346]
+a = [200, 300, 500, 800,900]
+b = [1.3155276775360107,1.529815673828125,1.8073720932006836, 2.2616989612579346,3.225600004196167]
+d = [0.24578857421875,0.5436396598815918,1.544672966003418,3.963372230529785,8.024326086044312]
 plt.xlabel('Image Size')
 plt.ylabel('Runtime')
 
-plt.plot(a,b)
-
-# sequencial
-c = [200, 300, 500, 800]
-d = [0.24578857421875,0.5436396598815918,1.544672966003418,3.963372230529785]
-plt.xlabel('Image Size')
-plt.ylabel('Runtime')
-
-plt.plot(c,d)
+plt.plot(a,b,label="Parallel")
+plt.plot(a,d, label = "Sequencial")
+plt.legend()
