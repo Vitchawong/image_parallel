6ec57341b084eaf2835329f8ba91b2cf8b212b35
Vitchawong <118838766+Vitchawong@users.noreply.github.com>
2023-05-17 15:40:34 +0700

    Update edge_multiprocess.py

--- edge_multiprocess.py
+++ edge_multiprocess.py
@@ -7,49 +7,56 @@ import time
 from multiprocessing import Process, shared_memory
 
 if __name__ == "__main__":
-    # FNAME = "blur_img.jpg"
+    # image_name = "blur_img.jpg"
     image_name = "dog.jpg"
     KERNELS = {"Edge Detection": np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]), "Sharpen": np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])}
     kernel = KERNELS["Edge Detection"]
   
-
+    # make sure the image is black and white
     image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-    image = cv2.resize(image, (500, 500))
-    image = np.pad(image, [(1, 1), (1, 1)], 'constant', constant_values=0)
+    # image size
+    image = cv2.resize(image, (800, 800))
+    print(image)
 
+    # Extend image size (boundary condition handling)
+    image = np.pad(image, [(1, 1), (1, 1)], 'constant', constant_values=0)
+    padded_img = image
+    print(padded_img)
+
+
+    # display image   
     cv2.imshow("image", image)
+    
+    # display original image until a key is pressed
     cv2.waitKey(0)
-    print(os.getpid())
+
 
     final_image = np.zeros(shape=(image.shape[0], image.shape[1]))
-
-    num_processes = 1
+    num_processes = 6
     rows_n = int(image.shape[0]/num_processes)
-
     process_stored = []
 
 
-
-    shm = shared_memory.SharedMemory(create=False, size=final_image.nbytes, name="shr_mem")
+    # initialize memory
+    shm = shared_memory.SharedMemory(create=True, size=final_image.nbytes, name="shr_mem")
     final_image = np.ndarray(image.shape, dtype=np.float32, buffer=shm.buf)
 
 
     for i in range(num_processes):                                                            #row_start, row_end, col_start, col_end, kernel
         process_stored.append(Process(target=convolution.convolve_multi_process, args=(image, i*rows_n, (i+1)*rows_n, 1, image.shape[1]-1, kernel)))
+        
 
     start = time.time()
-    print(start)
     for i in process_stored:
         i.start()
+        
 
     for i in process_stored:
         i.join()
 
     end = time.time()
     print("time taken: ", end-start)
-
     cv2.imshow("image", final_image/255)
     cv2.waitKey(0)
-
-    cv2.imwrite("FNAME",final_image)
+    cv2.imwrite(image_name,final_image)
+    
